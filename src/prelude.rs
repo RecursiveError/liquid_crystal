65a2d857b5bf511d1640dc20554dda0e0db4b6ed
guilherme Silva Schultz <guilhermeSschultz.contato@gmail.com>
2023-01-12 21:07:24 -0300

    update prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,3 +3,4 @@ pub use crate::lcd_trait::SendType::*;
 pub use crate::lcd_trait::Commands::*;
 pub use crate::lcd_trait::I2C_ADDRESS;
 pub use crate::lcd_trait::DEFAULT_CONFIG;
+pub use crate::lcd_trait::dummy::Dummy as lcd_dummy;
